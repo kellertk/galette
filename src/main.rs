8c40f11d6ee91e8bbc1450cd98e3ba23f8e64e93
Tom Keller <tom@tompkel.net>
2026-05-09 18:42:32 -0700

    chore: migrate to clap::_derive

--- src/main.rs
+++ src/main.rs
@@ -6,68 +6,56 @@
 // compatible with galette's.
 //
 
-extern crate clap;
-extern crate galette;
-
-use clap::{Arg, ArgAction, Command};
-
 use std::process;
+
+use clap::Parser;
 
 use galette::writer;
 
-fn main() {
-    let matches = Command::new("Galette")
-        .version(env!("CARGO_PKG_VERSION"))
-        .author("Simon Frankau <sgf@arbitrary.name>")
-        .about("GALasm-compatible GAL assembler")
-        .arg(Arg::new("INPUT.pld").help("Input file").required(true))
-        .arg(
-            Arg::new("secure")
-                .short('s')
-                .long("secure")
-                .help("Enable security fuse")
-                .action(ArgAction::SetTrue),
-        )
-        .arg(
-            Arg::new("nochip")
-                .short('c')
-                .long("nochip")
-                .help("Disable .chp file output")
-                .action(ArgAction::SetTrue),
-        )
-        .arg(
-            Arg::new("nofuse")
-                .short('f')
-                .long("nofuse")
-                .help("Disable .fus file output")
-                .action(ArgAction::SetTrue),
-        )
-        .arg(
-            Arg::new("nopin")
-                .short('p')
-                .long("nopin")
-                .help("Disable .pin file output")
-                .action(ArgAction::SetTrue),
-        )
-        .arg(
-            Arg::new("ptd")
-                .long("ptd")
-                .help("Set product term disable bits (16V8/20V8 only)")
-                .action(ArgAction::SetTrue),
-        )
-        .get_matches();
+#[derive(Parser)]
+#[command(
+    name = "Galette (galette-tk)",
+    version,
+    about = "GALasm-compatible GAL assembler"
+)]
+struct Cli {
+    /// Input file
+    #[arg(name = "INPUT.pld")]
+    input: String,
 
-    let file_name = matches.get_one::<String>("INPUT.pld").unwrap();
+    /// Enable security fuse
+    #[arg(short, long)]
+    secure: bool,
+
+    /// Disable .chp file output
+    #[arg(short = 'c', long)]
+    nochip: bool,
+
+    /// Disable .fus file output
+    #[arg(short = 'f', long)]
+    nofuse: bool,
+
+    /// Disable .pin file output
+    #[arg(short = 'p', long)]
+    nopin: bool,
+
+    /// Set product term disable bits (16V8/20V8 only)
+    #[arg(long)]
+    ptd: bool,
+}
+
+fn main() {
+    let cli = Cli::parse();
 
     let config = writer::Config {
-        gen_fuse: !matches.get_flag("nofuse"),
-        gen_chip: !matches.get_flag("nochip"),
-        gen_pin: !matches.get_flag("nopin"),
-        jedec_sec_bit: matches.get_flag("secure"),
-        disable_unused_pt: matches.get_flag("ptd"),
+        gen_fuse: !cli.nofuse,
+        gen_chip: !cli.nochip,
+        gen_pin: !cli.nopin,
+        jedec_sec_bit: cli.secure,
+        disable_unused_pt: cli.ptd,
     };
 
-    if let Err(e) = galette::assemble(file_name, &config) {
+    if let Err(e) = galette::assemble(&cli.input, &config) {
         eprintln!("{}", e);
         process::exit(1);
     }
